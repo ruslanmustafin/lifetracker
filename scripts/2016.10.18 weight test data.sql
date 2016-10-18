56f2cffdd4bd8b4c6f18dafeb424b3307d912f3f
Oleg.Ignatov <oleg.b.ignatov@gmail.com>
2016-10-18 14:27:03 +0300

    added a script to add a test user Ivan Vesov and data for his weight measurements for the first 10 months of 2016

---
+++ scripts/2016.10.18 weight test data.sql
@@ -0,0 +1,112 @@
+﻿INSERT INTO "USER" (email, password, first_name, last_name)
+	VALUES ('weight@test.com', '123', 'Ivan', 'Vesov');
+
+-- January
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.01.01',
+		90
+		);
+
+-- February
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.02.01',
+		92
+		);
+
+-- March
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.03.01',
+		93
+		);
+
+-- April
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.04.01',
+		95
+		);
+
+-- May
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.05.01',
+		92
+		);
+
+-- June
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.06.01',
+		90
+		);
+
+-- July
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.07.01',
+		89
+		);
+
+-- August
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.08.01',
+		88
+		);
+
+-- September
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.09.01',
+		90
+		);
+
+-- October
+INSERT INTO "WEIGHT" ("user", weight_date_time, value)
+	VALUES (
+		(SELECT user_id
+		FROM "USER"
+		WHERE email = 'weight@test.com'
+		LIMIT 1),
+		'2016.10.01',
+		92
+		);
