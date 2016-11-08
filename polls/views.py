9a69bf073ba4c427566667debe8499bbc86ab879
Oleg.Ignatov <oleg.b.ignatov@gmail.com>
2016-11-08 13:56:41 +0300

    fixed transfering strings with quotes to a template

--- polls/views.py
+++ polls/views.py
@@ -78,11 +78,11 @@ def weight(request):
     weightsString = ''
     weights = Weight.objects.filter(user=request.user.id).order_by('weight_date_time')
     for weight in weights:
-        weightsString += '[' + str(weight.weight_date_time.date()) + ',' + str(weight.value) + ']' + ','
+        weightsString += '["' + str(weight.weight_date_time.date()) + '", ' + str(weight.value) + ']' + ', '
 
     return render(request, 'weight/detail.html',
                               {'weights': weights,
-                               'weightsString': weightsString[:-1]})
+                               'weightsString': weightsString[:-2]})
 
 
 def nutrition(request):
