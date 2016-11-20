17a8ffc1aace72833233f6c47ffd2aa4ab8eb622
soberdeer <rarerial@mail.ru>
2016-11-20 17:41:51 +0300

    some more corrections

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -28,30 +28,3 @@ function weightScripts() {
         })
     });
 }
-
-function nutritionScripts() {
-    $().ready(function () {
-        $('#weight-table').on('click', '.remove-weight-btn', function (e) {
-            var weightId = $(this).data('weightId');
-            e.preventDefault();
-            $.ajax({
-                type: "DELETE",
-                //dataType: "jsonp",
-                url: '/api/weight/' + weightId,
-                success: function (data) {
-                    $(this).parents('tr').fadeOut().remove();
-                    console.log(data);
-                }
-            });
-        });
-        diff = 0;
-        weightBuff = chartData[0][2];
-        $.each(chartData, function (index, value) {
-            diff = value[2] - weightBuff;
-            $('#weight-table').append(
-                '<tr><td>' + value[1] + '</td><td>' + value[2] + '</td><td><a href="#" data-weight-id=' + value[0] + ' class="remove-weight-btn"><span class="glyphicon glyphicon-trash"></span></a></td><td>' + diff + '</td></tr>'
-            );
-            weightBuff = value[2];
-        })
-    });
-}
