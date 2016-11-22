b422bab2ef1b7581405c8375295b9f65477d8155
Oleg.Ignatov <oleg.b.ignatov@gmail.com>
2016-11-22 19:51:01 +0300

    added a redirect to 404.html in case a page is not found

--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from models import Weight
 from models import Meal
 from models import Exercise
 
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
 
 def login_view(request):
     return render(request, 'index-unreg.html', {})
